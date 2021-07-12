8c4e7f1632201ca99a1577ed4336d10c2f01cf05
joon <michaelhj123@gmail.com>
2021-07-13 03:38:55 +0900

    adding my files to my repository

---
+++ app.js
@@ -0,0 +1,10 @@
+// const를 기본적으로 사용하고
+// 업데이트를 해야하는 부분은 letdmf tkdyd
+// false는 값이 존재하는 것. null은 값은 존재하지만 아무것도 아닌 것
+// undefine는 아무것도 정의되지 않은 것
+
+const hello = () => {
+    console.log('hello');
+}
+
+hello();
